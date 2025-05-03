bfb5d72d378bc7b9e069c9f2bdee7889a6fb0b09
Riddhima Gupta <griddhima226@gmail.com>
2025-05-03 14:54:20 +0530

    Initial commit

---
+++ lib/Presentation/splash.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:grabber/Widgets/nav_bar.dart';
+import 'package:lottie/lottie.dart';
+class Splash  extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState(){
+    super.initState();
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=>NavBar()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    final containerWidth = screenWidth * .5;
+    final containerWidth1 = screenWidth * .771;
+    final containerHeigth = screenHeight * .10;
+    final containerHeigth1 = screenHeight * .267;
+    final top = screenHeight * .58;
+    final top1 = screenHeight * .3;
+    final left = screenWidth * .25;
+    final left2 = screenWidth * .132;
+    final fontsize = screenWidth * .128;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+
+          Positioned(
+            top:top1 ,
+            left:left2 ,
+            child: SizedBox(
+                height:containerHeigth1,
+                width: containerWidth1,
+                child: Lottie.asset("Assets/Lottie/Animation - 1746261786846.json")),
+          ),
+          Positioned(
+              top: top,
+              left: left,
+              child: Container(
+                width: containerWidth,
+                height: containerHeigth,
+                child: Text(
+                  "Grabber",
+                  style: GoogleFonts.balooDa2(
+                      fontWeight: FontWeight.w800,
+                      fontSize: fontsize,
+                      height: 1,
+
+                      color: Color(0XFF0CA201)),
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Widgets/nav_bar.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../Presentation/favourite.dart';
+import '../Presentation/homepage.dart';
+import '../Presentation/menu.dart';
+import '../Presentation/profile.dart';
+import '../Presentation/search.dart';
+
+class NavBar extends StatefulWidget {
+  const NavBar({super.key});
+
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  final PageController _pageViewController = PageController();
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    final iconsize = screenWidth * .065;
+    final navbarheigth = screenWidth * .23;
+
+    final fontsize = screenWidth * .03;
+
+    return Scaffold(
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: _pageViewController,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        children: [
+          Home(),
+          Favourite(),
+          Search(),
+          Profile(),
+          Menu(),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        height: navbarheigth,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border:
+                Border(top: BorderSide(color: Color(0xFFF0EFEF), width: 1))),
+        child: BottomNavigationBar(
+            backgroundColor: Colors.white,
+            iconSize: iconsize,
+            selectedLabelStyle: GoogleFonts.inter(
+              fontWeight: FontWeight.w600,
+              fontSize: fontsize,
+            ),
+            unselectedLabelStyle: GoogleFonts.inter(
+              fontWeight: FontWeight.w500,
+              fontSize: fontsize,
+            ),
+            selectedItemColor: Color(0xFF0CA201),
+            unselectedItemColor: Color(0xFF000000),
+            currentIndex: _selectedIndex,
+            type: BottomNavigationBarType.fixed,
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+              _pageViewController.jumpToPage(index);
+            },
+            items: [
+              BottomNavigationBarItem(
+                  icon: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.home_outlined),
+                      SizedBox(
+                        height: screenWidth * .01,
+                      ),
+                    ],
+                  ),
+                  label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.favorite_border),
+                      SizedBox(
+                        height: screenWidth * .01,
+                      ),
+                    ],
+                  ),
+                  label: 'Favourite'),
+              BottomNavigationBarItem(
+                  icon: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.search_rounded),
+                      SizedBox(
+                        height: screenWidth * .01,
+                      ),
+                    ],
+                  ),
+                  label: 'Search'),
+              BottomNavigationBarItem(
+                  icon: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.person_outline),
+                      SizedBox(
+                        height: screenWidth * .01,
+                      ),
+                    ],
+                  ),
+                  label: 'Profile'),
+              BottomNavigationBarItem(
+                  icon: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.menu),
+                      SizedBox(
+                        height: screenWidth * .01,
+                      ),
+                    ],
+                  ),
+                  label: 'Menu'),
+            ]),
+      ),
+    );
+  }
+}
